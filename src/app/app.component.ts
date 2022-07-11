b2e21ed3f548c06f9fc4bae89b77417f8f1fadab
Krystian <krystianpatryas9@gmail.com>
2022-07-11 18:14:11 +0200

    new features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { decimalDigest } from '@angular/compiler/src/i18n/digest';
 import { Component } from '@angular/core';
 import { Task } from './task';
 
@@ -8,22 +7,22 @@ import { Task } from './task';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  config: { [key: string]: string | null };
+  taskName = 'Sugerowane zadanie codzienne: odkurzanie';
+  taskDate = '';
+  config: { [key: string]: string } | null  = null;
   tasks: Task[] = [
     {
       name: 'Siłownia',
       deadline: '2020-01-02',
       done: false,
     },
-
     {
       name: 'Nauka Angulara',
       deadline: '2020-01-03',
       done: false,
     },
-
     {
-      name: 'Sprzątanie',
+      name: 'Sprzątanie kuwety',
       deadline: '2020-01-04',
       done: false,
     },
@@ -33,21 +32,24 @@ export class AppComponent {
     setTimeout(() => {
       this.config = {
         title: 'Lista zadań',
-        footer: 'Lista zadań zbudowana w Angularze',
+        footer: '© Lista zadań zbudowana w Angularze.',
         date: new Date().toDateString(),
       };
     }, 500);
   }
+
   clearTasks() {
     this.tasks = [];
   }
 
-  createTask(name: string, deadline: string) {
+  createTask() {
     const task: Task = {
-      name: name,
-      deadline: deadline,
+      name: this.taskName,
+      deadline: this.taskDate,
       done: false,
     };
     this.tasks.push(task);
+    this.taskName = '';
+    this.taskDate = '';
   }
 }
