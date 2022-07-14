65889446464e5ad51675a57cc339f01bbe4e3a68
Krystian <krystianpatryas9@gmail.com>
2022-07-14 20:15:21 +0200

    last features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,6 +37,7 @@ export class AppComponent {
         date: new Date().toDateString(),
       };
     }, 500);
+    this.sortTasks();
   }
 
   clearTasks() {
@@ -52,9 +53,24 @@ export class AppComponent {
     this.tasks.push(task);
     this.taskName = '';
     this.taskDate = '';
+    this.sortTasks();
   }
 
   switchEditMode() {
     this.editMode = !this.editMode;
+  }
+
+  markTaskAsDone(task: Task) {
+    task.done = true;
+    this.sortTasks();
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter(e => e !== task);
+    this.sortTasks();
+  }
+
+  private sortTasks() {
+    this.tasks = this.tasks.sort((a: Task, b: Task) => a.done === b.done ? 0 : a.done ? 1 : -1)
   }
 }
